a75fd8d134f532ca3a21c7070b6fa747f8d1cdce
c081ffw <c081ffw@yamaguchi-u.ac.jp>
2025-07-03 18:40:04 +0900

    レビュー機能の追加

---
+++ class_2025_b/lib/states/review_state.dart
@@ -0,0 +1,78 @@
+import 'package:class_2025_b/states/user_state.dart';
+import 'package:flutter/widgets.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:class_2025_b/services/database_service.dart';
+import 'package:class_2025_b/models/review_model.dart';
+import 'package:class_2025_b/states/recipe_id_state.dart';
+
+
+// レビューが投稿されるごとに発火するプロバイダ
+final reviewTriggerProvider = StateProvider<int>((ref) {
+  // レビューのトリガー状態を管理
+  return 0;
+});
+
+// レビューの状態を管理するプロバイダ
+final reviewProvider = FutureProvider<Review>((ref) async {
+
+  final recipeId = ref.watch(recipeIdProvider); // レシピIDを監視して再構築を促す
+
+  final dbService = DatabaseService();
+
+  if(recipeId == null || recipeId.isEmpty) {
+    debugPrint('Recipe ID is null or empty, returning empty review');
+    return Review.empty; // レシピIDが無い場合は空のレビューを返す
+  }
+
+  final user = ref.watch(userProvider);
+  if (user == null) {
+    return Review.empty;
+  }
+
+  final userId = user.uid;
+
+
+  if (recipeId == null || recipeId.isEmpty) {
+    throw Exception('Recipe ID is null or empty');
+  }
+
+  // レビューをデータベースから取得
+  final Review review = await dbService.getReviewByRecipeIdAndUserId(userId, recipeId);
+
+  debugPrint('Fetched review: $review for user $userId and recipe $recipeId');
+
+  return review;
+});
+
+Future<void> addReview(int tasteRating, int easeRating, int costRating, WidgetRef ref) async {
+
+  final user = ref.read(userProvider);
+
+  // ユーザーがログインしているか確認
+  if (user == null) {
+    throw Exception('ログインしていません');
+  }
+
+  final userId = user.uid;
+
+  final recipeId = ref.read(recipeIdProvider);
+
+  if (recipeId == null || recipeId.isEmpty) {
+    throw Exception('レシピIDが取得できていません');
+  }
+
+  // DBサービスのインスタンスを作成
+  final dbService = DatabaseService();
+
+  // reviewインスタンスの作成
+  final review = Review(
+    tasteRating: tasteRating,
+    easeRating: easeRating,
+    costRating: costRating,
+  );
+
+  // レビューをデータベースに追加
+  await dbService.addReview(userId, recipeId, review);
+
+  return;
+}
--- class_2025_b/lib/widgets/review_widget.dart
+++ class_2025_b/lib/widgets/review_widget.dart
@@ -1,6 +1,82 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:class_2025_b/states/review_state.dart';
+import 'package:class_2025_b/models/review_model.dart';
+import 'package:class_2025_b/states/user_state.dart';
+
+class ReviewFormWidget extends HookConsumerWidget {
+  final Review review;
+
+  const ReviewFormWidget({super.key, required this.review});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+
+    final tasteRating = useState(review.tasteRating);
+    final easeRating  = useState(review.easeRating);
+    final costRating  = useState(review.costRating);
+
+    useEffect(() {
+      tasteRating.value = review.tasteRating;
+      easeRating.value  = review.easeRating;
+      costRating.value  = review.costRating;
+      return null;
+    }, [review]);
+
+    final user = ref.watch(userProvider);
+
+    final bool isValid = tasteRating.value > 0 && easeRating.value > 0 && costRating.value > 0 && user != null;
+
+    final header = Container(
+      // padding: const EdgeInsets.all(16.0),
+      child: Text(
+        'レビュー',
+        style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+
+    return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      // Center(child: 
+      header,
+      // ),
+      SizedBox(height: 16),
+      ReviewRatingRow(label: '味', notifier: tasteRating),
+      SizedBox(height: 16),
+      ReviewRatingRow(label: '作りやすさ', notifier: easeRating),
+      SizedBox(height: 16),
+      ReviewRatingRow(label: 'コスパ', notifier: costRating),
+      SizedBox(height: 16),
+      isValid ? SizedBox.shrink() :
+      Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Text(
+          'レビューを投稿するには、ログインした状態すべての評価を設定してください',
+          style: TextStyle(color: Colors.red),
+        ),
+      ),
+      Center(
+        child: ElevatedButton(
+          onPressed: isValid ? () async {
+            await addReview(
+              tasteRating.value,
+              easeRating.value,
+              costRating.value,
+              ref
+            );
+          } : null,
+          child: const Text('レビューを投稿する'),
+        )
+      )
+    ],
+  );
+  }
+}
 
 class ReviewWidget extends HookConsumerWidget {
 
@@ -8,46 +84,41 @@ class ReviewWidget extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        ReviewRatingRow(label: '味'),
-        SizedBox(height: 16),
-        ReviewRatingRow(label: '作りやすさ'),
-        SizedBox(height: 16),
-        ReviewRatingRow(label: 'コスパ'),
-        SizedBox(height: 16),
-        Center(
-          child: ElevatedButton(
-            onPressed: null, 
-            child: const Text('レビューを投稿する'),
-          )
-        )
-      ],
+
+    final asyncReview = ref.watch(reviewProvider);
+
+    return asyncReview.when(
+      data: (review) => ReviewFormWidget(review: review),
+      loading: () => ReviewFormWidget(review: Review.empty),
+      error: (error, stack) => Center(child: Text('Error: $error')),
     );
   }
 }
 
 
 class ReviewRatingRow extends HookConsumerWidget {
-  final String label;
 
-  const ReviewRatingRow({super.key, required this.label});
+  final String label;
+  final ValueNotifier<int> notifier;
+
+  const ReviewRatingRow({super.key, required this.label, required this.notifier});
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final rating = useState(0);
+
+    debugPrint(notifier.value.toString());
+
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
         SizedBox(width: 80, child: Text(label, overflow: TextOverflow.ellipsis)),
         ...List.generate(5, (i) => IconButton(
           icon: Icon(
-            i < rating.value ? Icons.star : Icons.star_border,
+            i < notifier.value ? Icons.star : Icons.star_border,
             color: Colors.amber,
             size: 18,
           ),
           onPressed: () {
-            rating.value = i + 1;
+            notifier.value = i + 1;
           },
           iconSize: 18,
           padding: EdgeInsets.all(-20), // マイナスパディングでさらに詰める
@@ -55,7 +126,7 @@ class ReviewRatingRow extends HookConsumerWidget {
           visualDensity: VisualDensity(horizontal: -4, vertical: -4),
         )),
         SizedBox(width: 2),
-        Text(rating.value > 0 ? rating.value.toString() : ''),
+        Text(notifier.value > 0 ? notifier.value.toString() : ''),
       ],
     );
   }
