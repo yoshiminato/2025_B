be7188675a0c17bd2e5404f24e82070e48b941c8
c081ffw <c081ffw@yamaguchi-u.ac.jp>
2025-07-05 12:27:41 +0900

    検索結果にレビュー平均を反映

--- class_2025_b/lib/models/review_model.dart
+++ class_2025_b/lib/models/review_model.dart
@@ -1,13 +1,13 @@
 class Review {
   final int tasteRating;
   final int easeRating;
-  final int costRating;
+  final int cospRating;
   final int uniquenessRating;
 
   Review({
     required this.tasteRating,
     required this.easeRating,
-    required this.costRating,
+    required this.cospRating,
     required this.uniquenessRating
   });
 
@@ -18,7 +18,7 @@ class Review {
       'recipeId': recipeId,
       'tasteRating': tasteRating,
       'easeRating': easeRating,
-      'costRating': costRating,
+      'cospRating': cospRating,
       'uniquenessRating': uniquenessRating, // 追加: ユニークさの評価
     };
   }
@@ -28,7 +28,7 @@ class Review {
     return Review(
       tasteRating: map['tasteRating'] ?? 0,
       easeRating: map['easeRating'] ?? 0,
-      costRating: map['costRating'] ?? 0,
+      cospRating: map['cospRating'] ?? 0,
       uniquenessRating: map['uniquenessRating'] ?? 0,
     );
   }
@@ -36,8 +36,32 @@ class Review {
   static get empty => Review(
     tasteRating: 0,
     easeRating: 0,
-    costRating: 0,
+    cospRating: 0,
     uniquenessRating: 0,
   );
 
+}
+
+class ReviewAverage {
+  final double tasteAve;
+  final double easeAve;
+  final double cospAve;
+  final double uniquenessAve;
+  final double reccommend;
+
+  ReviewAverage({
+    required this.tasteAve, // 味の平均評価
+    required this.easeAve, // 簡単さの平均評価
+    required this.cospAve, // コストパフォーマンスの平均評価
+    required this.uniquenessAve, // ユニークさの平均評価
+    required this.reccommend, // 追加: おすすめ度の平均
+  });
+
+  static get empty => ReviewAverage(
+    tasteAve: 0.0, // 味の初期値
+    easeAve: 0.0, // 簡単さの初期値
+    cospAve: 0.0, // コストパフォーマンスの初期値
+    uniquenessAve: 0.0, // ユニークさの初期値
+    reccommend: 0.0, // おすすめ度の初期値
+  );
 }
